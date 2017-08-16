c2ebd6afcce43b978ab76e4bb0732b892f3088c1
ocius <mamedzade030@gmail.com>
2017-08-16 23:18:09 +0400

    Created VUE computed property newManat

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ new Vue({
     mammad :0,
     shirvan :0,
     momunaxatun : 0
+
+  },
+  
+  computed:{
+    newManat:function(){
+      var oldSum=(this.nizami * 500) + (this.mammad * 1000) + (this.shirvan * 10000) + (this.momunaxatun * 50000);
+      return  oldSum / 5000;
+    }
   }
 
 });
